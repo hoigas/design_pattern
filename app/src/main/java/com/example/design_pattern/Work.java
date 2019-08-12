1a6dce7886cfa52a55c5622fe337c9d66bbdfec6
HanSeongmin <smhan@hdtel.co.kr>
2019-08-12 23:38:43 +0900

    work2 - fail, success

--- app/src/main/java/com/example/design_pattern/Work.java
+++ app/src/main/java/com/example/design_pattern/Work.java
@@ -2,6 +2,6 @@ package com.example.design_pattern;
 
 public class Work {
     public String work1;
-    public String work2 = "success";
+    public String work2 = "fail success";
     public String work3;
 }
